51e5082d4f88bbeb8b36c74cc2a6963a86ae6244
Pierre FILSTROFF <pierre.filstroff@gmail.com>
2023-04-07 10:21:11 +0200

    Revert "Added overlaping checks for booking and reservations"
    This reverts commit ee7b183c039972d4d247f99c271b5d2a11bdee2c.

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -6,11 +6,6 @@ class Reservation < ApplicationRecord
   validates :end_date, presence: true
   validates_date :start_date, before: :end_date, on_or_after: lambda { Date.current }
   validate :booking_available, if: -> { self.listing.present? }
-  validate :check_overlapping
-
-  scope :overlaps, -> (start_date, end_date) do
-    where "((start_date <= ?) and (end_date >= ?))", end_date, start_date
-  end
 
   state_machine :state, initial: :active do
     event :cancel do
@@ -27,12 +22,6 @@ class Reservation < ApplicationRecord
   after_save :create_checkout_checkin_missions, if: -> { self.saved_change_to_end_date? }
 
   private
-    def check_overlapping
-      if Reservation.overlaps(start_date, end_date).any?
-        errors.add(:start_date, "overlaps existing reservation")
-      end
-    end
-
     def booking_available
       unless self.listing.bookings.where("start_date <= ? AND end_date >= ?", self.start_date, self.end_date).exists?
         self.errors.add(:start_date, "is not available. No booking found for this date range.")
